86d6f65b2383f55f8477e085971492d665091736
NelluriMahitha24 <mahithachowdary824@gmail.com>
2024-10-01 12:55:28 +0530

    files

---
+++ basics/poly/Account.java
@@ -0,0 +1,9 @@
+class Account{
+    private String acc_name;
+    private String acc_email;
+    public Account(String name,String email){
+        this.acc_name=name;
+        this.acc_email=email;
+    }
+    public void bal(){};
+}
---
+++ basics/poly/bank.java
@@ -0,0 +1,4 @@
+abstract class Bank{
+   public abstract void bal(); 
+}
+
